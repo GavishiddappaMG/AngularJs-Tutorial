1189b0eb7f790c687a5e21169389bee9e4167eb4
Gavishiddappa <gmgmadagi@gmail.com>
2017-05-11 02:47:48 +0530

    Second Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsTutorialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularJsTutorialApp
+ */
+angular.module('angularJsTutorialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsTutorialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsTutorialApp
+ */
+angular.module('angularJsTutorialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
